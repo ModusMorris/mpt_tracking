335a8f108ce079ec90fc7c750518caeded5eaed5
anna <anna@anna.popova.de>
2024-07-04 16:58:13 +0200

    only scripts for Prob3

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import numpy as np
 import dummy
 import constant_position
 import random_noise
+import angular
 
 # TODO: Add your filters here
 filters = {
@@ -18,6 +19,11 @@ filters = {
         "color": [0.5, 0.1, 0.9],
         "constantposition": constant_position.ConstantPositionFilter(),
         "randomnoise": random_noise.RandomNoiseFilter(2),
+        "constantvelocity": dummy.DummyFilter(2),
+        "constantvelocity2": dummy.DummyFilter(2),
+        "constantturn": dummy.DummyFilter(2),
+        "angular": angular.AngularFilter(2),
+
     },
 }
 if __name__ == "__main__":
