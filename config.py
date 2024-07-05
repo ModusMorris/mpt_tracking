059b6a278a44aa5075dc716dee7916c6275b1b8c
anna <anna@anna.popova.de>
2024-07-06 01:59:11 +0200

    updated config.py and all the measures to avoid merge conflict

--- config.py
+++ config.py
@@ -3,13 +3,16 @@ import dummy
 import nam
 import random_noise
 import angular
+import constantvelocity
+import constantvelocity2
 import constant_turn
+
 
 # TODO: Add your filters here
 filters = {
     "Dummy": {
         "color": [0.2, 0.2, 0.4],
-        "constantposition": dummy.DummyFilter(),
+        "constantposition": dummy.DummyFilter(2),
         "constantvelocity": dummy.DummyFilter(2),
         "constantvelocity2": dummy.DummyFilter(2),
         "constantturn": dummy.DummyFilter(2),
@@ -17,13 +20,13 @@ filters = {
         "angular": dummy.DummyFilter(2),
     },
     "NAMTeam": {
-        "color": [0.5, 0.1, 0.9],
         "constantposition": nam.NamFilter(2),
         "randomnoise": random_noise.RandomNoiseFilter(2),
-        "constantvelocity": dummy.DummyFilter(2),
-        "constantvelocity2": dummy.DummyFilter(2),
+        "constantvelocity": constantvelocity.KalmanFilterConstantVelocity(),
+        "constantvelocity2": constantvelocity2.KalmanFilterConstantVelocityMultiple(),
         "constantturn": constant_turn.ConstantTurnFilter(2),
         "angular": angular.AngularFilter(2),
+
 
     },
 }
